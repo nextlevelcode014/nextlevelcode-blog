cef7f217f106ac793a1e1e53ebe9ba4e81aadc47
m4rc3l04ugu2t0 <mrcelo014@gmail.com>
2025-02-21 23:37:45 -0300

    production

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  env: {
-    SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  },
   images: {
     remotePatterns: [
       {

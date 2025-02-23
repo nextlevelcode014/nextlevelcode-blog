46931c351eb97a1cbead69a27d64d3d47d9ade6e
m4rc3l04ugu2t0 <mrcelo014@gmail.com>
2025-02-22 21:51:36 -0300

    production

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   env: {
     SANITY_DATASET: process.env.SANITY_DATASET,
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    API_URL: process.env.API_URL,
+    API_KEY: process.env.API_KEY,
   },
   crossOrigin: 'anonymous',
   images: {
